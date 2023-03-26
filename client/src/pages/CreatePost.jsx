1ebc67e9c98354546471fc905f1dd3b8b06132e7
yashmahajan29 <yashsmahajan82@gmail.com>
2023-03-26 21:34:43 +0530

    image generated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,29 +18,29 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false);
 
   const generateImage = async () => {
-    // if (form.prompt) {
-    //   try {
-    //     setGeneratingImg(true);
-    //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({
-    //         prompt: form.prompt,
-    //       }),
-    //     });
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
 
-    //     const data = await response.json();
-    //     setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-    //   } catch (err) {
-    //     alert(err);
-    //   } finally {
-    //     setGeneratingImg(false);
-    //   }
-    // } else {
-    //   alert('Please provide proper prompt');
-    // }
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
   };
 
   const handleSubmit = async (e) => {
