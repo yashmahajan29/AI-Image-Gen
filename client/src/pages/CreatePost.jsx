a396db1b371a620cbc4b379a7e105f40023351f1
yashmahajan29 <yashsmahajan82@gmail.com>
2023-03-26 23:37:02 +0530

    done1

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -44,30 +44,30 @@ const CreatePost = () => {
   };
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
 
-    // if (form.prompt && form.photo) {
-    //   setLoading(true);
-    //   try {
-    //     const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //       },
-    //       body: JSON.stringify({ ...form }),
-    //     });
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
 
-    //     await response.json();
-    //     alert('Success');
-    //     navigate('/');
-    //   } catch (err) {
-    //     alert(err);
-    //   } finally {
-    //     setLoading(false);
-    //   }
-    // } else {
-    //   alert('Please generate an image with proper details');
-    // }
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
   };
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
